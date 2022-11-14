b0124d056903ef7e8ce98dbd53aea002a999fd5a
Omar Ahmed <developeromi@icloud.com>
2022-11-13 17:57:02 -0800

    Added transparency to the bars when pressed. Added a timer that returns the pressed bar to fully opaque.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,6 +11,15 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        
+        // Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        // Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            // Bring sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
     
     func playSound(soundName: String) {
